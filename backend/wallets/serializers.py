0c66fa1da5f8e0ca97ec88617a88a8f6a9e9bfcb
Sayid Mahmoudi <sayid.aban@gmail.com>
2024-04-10 15:47:23 +0330

    add deposit serializer

--- backend/wallets/serializers.py
+++ backend/wallets/serializers.py
@@ -8,3 +8,12 @@ class WalletSerializer(serializers.ModelSerializer):
         model = Wallet
         fields = ("uuid", "balance")
         read_only_fields = ("uuid", "balance")
+
+
+class DepositSerializer(serializers.Serializer):
+    amount = serializers.IntegerField()
+
+    def validate_amount(self, amount):
+        if amount <= 0:
+            raise serializers.ValidationError("value must be positive")
+        return amount
