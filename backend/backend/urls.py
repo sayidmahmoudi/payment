9b629a0c6ed40bedaa99ee0c94521145eeb4e2a4
Sayid Mahmoudi <sayid.aban@gmail.com>
2024-04-10 17:08:31 +0330

    add swagger

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Payment API",
+        default_version='v1',
+        description="Payment Backend",
+        contact=openapi.Contact(email="saeidmahmoudi79@gmail.com"),
+        license=openapi.License(name="BSD License")
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('wallets/', include("wallets.urls"))
+    path('wallets/', include("wallets.urls")),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
