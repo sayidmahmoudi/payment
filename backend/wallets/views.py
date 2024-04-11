3ba7bc6abc17c4c48276ceea5e70643caee6223b
Sayid Mahmoudi <sayid.aban@gmail.com>
2024-04-11 09:44:02 +0330

    add withdraw view

--- backend/wallets/views.py
+++ backend/wallets/views.py
@@ -4,10 +4,11 @@ from rest_framework import status
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
 
-from wallets.models import Wallet
-from wallets.serializers import WalletSerializer, DepositSerializer
-from wallets.services import DepositService
+from wallets.models import Wallet, Transaction
+from wallets.serializers import WalletSerializer, DepositSerializer, TransactionSerializer, WithdrawSerializer
+from wallets.services import DepositService, WithdrawService
 
 
 class CreateWalletView(CreateAPIView):
@@ -21,18 +22,28 @@ class RetrieveWalletView(RetrieveAPIView):
 
 
 class CreateDepositView(APIView):
+    @swagger_auto_schema(request_body=DepositSerializer)
     def post(self, request, uuid,  *args, **kwargs):
         serializer: DepositSerializer = DepositSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         amount: int = serializer.validated_data['amount']
-        wallet: Wallet = DepositService.deposit(wallet_uuid=uuid, amount=amount)
-        data: Dict = WalletSerializer(wallet).data
-        return Response(data=data, status=status.HTTP_200_OK)
+        transaction: Transaction = DepositService.deposit(wallet_uuid=uuid, amount=amount)
+        data: Dict = TransactionSerializer(transaction).data
+        return Response(data=data, status=status.HTTP_201_CREATED)
 
 
 class ScheduleWithdrawView(APIView):
-    def post(self, request, *args, **kwargs):
-        # todo: implement withdraw logic
-        pass
-        return Response({})
+    @swagger_auto_schema(request_body=WithdrawSerializer)
+    def post(self, request, uuid, *args, **kwargs):
+        serializer: WithdrawSerializer = WithdrawSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        amount: int = serializer.validated_data['amount']
+        execute_time = serializer.validated_data['execute_time']
+        transaction: Transaction = WithdrawService.create_transaction(
+            wallet_uuid=uuid,
+            amount=amount,
+            execute_time=execute_time
+        )
+        data: Dict = TransactionSerializer(transaction).data
+        return Response(data=data, status=status.HTTP_201_CREATED)
 
