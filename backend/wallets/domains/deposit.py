0254f8cc5c2a356fa87f83dd530d9819644e7dda
Sayid Mahmoudi <sayid.aban@gmail.com>
2024-04-11 09:46:19 +0330

    retuen transaction in deposit

--- backend/wallets/domains/deposit.py
+++ backend/wallets/domains/deposit.py
@@ -1,24 +1,26 @@
-from django.db import transaction
+from django.db import transaction as django_transaction
 from django.db.models import F
+from django.utils import timezone
 
 from wallets.models import Wallet, Transaction
 
 
 class DepositDomain:
-
     @classmethod
     def get_wallet_by_uuid(cls, wallet_uuid: str):
         return Wallet.objects.get(uuid=wallet_uuid)
 
     @classmethod
-    def deposit(cls, amount: int, wallet: Wallet) -> Wallet:
-        with transaction.atomic():
-            Transaction.objects.create(
+    def deposit(cls, amount: int, wallet: Wallet) -> Transaction:
+        with django_transaction.atomic():
+            transaction = Transaction.objects.create(
                 amount=amount,
+                wallet=wallet,
+                execute_time=timezone.now(),
                 type=Transaction.DEPOSIT,
                 state=Transaction.DONE,
             )
             wallet.balance = F("balance") + amount
             wallet.save()
             wallet.refresh_from_db()
-        return wallet
+        return transaction
--- backend/wallets/services/deposit.py
+++ backend/wallets/services/deposit.py
@@ -1,13 +1,13 @@
 from rest_framework.exceptions import NotFound
 
 from wallets.domains import DepositDomain
-from wallets.models import Wallet
+from wallets.models import Wallet, Transaction
 
 
 class DepositService:
 
     @classmethod
-    def deposit(cls, amount: int, wallet_uuid: str) -> Wallet:
+    def deposit(cls, amount: int, wallet_uuid: str) -> Transaction:
         try:
             wallet: Wallet = DepositDomain.get_wallet_by_uuid(wallet_uuid=wallet_uuid)
             return DepositDomain.deposit(amount=amount, wallet=wallet)
